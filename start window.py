6559704d3e9ff2b9d864ad40a1b5faed5d809746
NikaShat <nikusenka04@ya.ru>
2020-01-14 01:40:44 +0300

    ...

--- start window.py
+++ start window.py
@@ -1,23 +1,14 @@
 import pygame
 
-WIDTH = 600
-HEIGHT = 600
-
 
 class Start_Window():
     def __init__(self):
         pygame.init()
-        self.size = WIDTH, HEIGHT
+        self.size = 600, 600
         self.screen = pygame.display.set_mode(self.size)
 
     def draw(self):
         pass
 
 
-window = Start_Window()
-
-pygame.display.flip()
-
-while pygame.event.wait().type != pygame.QUIT:
-    pass
 pygame.quit()
